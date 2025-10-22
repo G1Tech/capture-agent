d852d0350ae41e2cd8525961da373073073ac31a
G1Tech <G1Tech@users.noreply.github.com>
2025-10-22 23:24:08 +0200

    init

---
+++ src/main/java/cc/g1cloud/captureagent/CaptureAgentApplication.java
@@ -0,0 +1,13 @@
+package cc.g1cloud.captureagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptureAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/g1cloud/captureagent/CaptureAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.g1cloud.captureagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptureAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
